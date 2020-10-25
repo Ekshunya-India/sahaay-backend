13b195df8c067c3a796ad06368c9f8e3cf937443
Sunil KS <sunilks.kavali@gmail.com>
2020-10-25 13:18:42 +0530

    Updated the API for Ticket Update.

--- src/main/java/com/ekshunya/sahaaybackend/handler/TicketTicketIdPutHandler.java
+++ src/main/java/com/ekshunya/sahaaybackend/handler/TicketTicketIdPutHandler.java
@@ -1,11 +1,18 @@
 package com.ekshunya.sahaaybackend.handler;
 
+import com.ekshunya.sahaaybackend.model.dtos.TicketDetailsUpdateDto;
+import com.ekshunya.sahaaybackend.model.dtos.TicketDto;
+import com.ekshunya.sahaaybackend.services.TicketServices;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 import com.networknt.handler.LightHttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,20 +20,32 @@ import java.util.Map;
 For more information on how to write business handlers, please check the link below.
 https://doc.networknt.com/development/business-handler/rest/
 */
+@Slf4j
 public class TicketTicketIdPutHandler implements LightHttpHandler {
 
     private final ObjectMapper objectMapper;
-    private final MongoDatabase mongoDatabase;
+    private final TicketServices ticketServices;
 
     @Inject
     public TicketTicketIdPutHandler(final ObjectMapper objectMapper,
-                                     final MongoDatabase mongoDatabase){
-        this.mongoDatabase = mongoDatabase;
+                                     final TicketServices ticketServices){
+        this.ticketServices = ticketServices;
         this.objectMapper = objectMapper;
     }
 
     @Override
-    public void handleRequest(HttpServerExchange exchange) throws Exception {
+    public void handleRequest(HttpServerExchange exchange) {
+        exchange.getRequestReceiver().receiveFullBytes((httpServerExchange, bytes) ->{
+            try {
+                TicketDetailsUpdateDto ticketDetailsUpdateDto = objectMapper.readValue(bytes,TicketDetailsUpdateDto.class);
+                TicketDto updatedTicket = this.ticketServices.updateTicket(ticketDetailsUpdateDto);
+                httpServerExchange.getResponseSender().send(this.objectMapper.writeValueAsString(updatedTicket));
+                httpServerExchange.setStatusCode(200);
+            } catch (IOException e) {
+                log.error(Arrays.toString(e.getStackTrace()));
+                httpServerExchange.setStatusCode(400);
+            }
+        });
         exchange.endExchange();
     }
 }
--- src/main/java/com/ekshunya/sahaaybackend/services/TicketServices.java
+++ src/main/java/com/ekshunya/sahaaybackend/services/TicketServices.java
@@ -1,6 +1,7 @@
 package com.ekshunya.sahaaybackend.services;
 
 import com.ekshunya.sahaaybackend.model.dtos.TicketCreateDto;
+import com.ekshunya.sahaaybackend.model.dtos.TicketDetailsUpdateDto;
 import com.ekshunya.sahaaybackend.model.dtos.TicketDto;
 import com.google.inject.Inject;
 import com.mongodb.reactivestreams.client.MongoDatabase;
@@ -16,6 +17,11 @@ public class TicketServices {
 
 	public TicketDto createTicket(final TicketCreateDto ticketCreateDto) {
 		//TODO connect with MongoDB Ticket Collection and create the Ticket.
+		return null;
+	}
+
+	public TicketDto updateTicket(final TicketDetailsUpdateDto ticketDetailsUpdateDto) {
+		//TODO update the Ticket in Mongo DB using the Async Drivers. Return value might change here as Mongo might return a Publisher.
 		return null;
 	}
 }
