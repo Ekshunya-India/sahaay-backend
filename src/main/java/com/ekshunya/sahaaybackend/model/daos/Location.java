898db63f795df0790cbfacf3b9156e37263bb091
Sunil KS <sunilks.kavali@gmail.com>
2020-10-30 09:06:26 +0530

    Changed the location class to have Mongo point and Type given by mongo DB so that we can make use of GeoPoistion class and better query result after indexing.

--- src/main/java/com/ekshunya/sahaaybackend/model/daos/Location.java
+++ src/main/java/com/ekshunya/sahaaybackend/model/daos/Location.java
@@ -1,13 +1,20 @@
 package com.ekshunya.sahaaybackend.model.daos;
 
+import com.mongodb.client.model.geojson.GeoJsonObjectType;
+import com.mongodb.client.model.geojson.Point;
+import com.mongodb.client.model.geojson.Position;
 import lombok.Value;
 
 import javax.validation.constraints.NotNull;
 
 @Value
-public class Location {
-	@NotNull(message = "longitude cannot be null")
-	double lng;
-	@NotNull(message = "latitude cannot be null")
-	double lat;
+public class Location  {
+	@NotNull(message = "A location must have a type for it. Perhapes a Point?")
+	GeoJsonObjectType type;
+	Point point;
+
+	public Location(final double lng, final double lat){
+		this.type= GeoJsonObjectType.POINT;
+		this.point = new Point(new Position(lng,lat));
+	}
 }
