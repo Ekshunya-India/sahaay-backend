ea59f4491a45313f068c1e9dc18769e175e15bb7
Sunil KS <sunilks.kavali@gmail.com>
2020-11-01 17:01:49 +0530

    Replaced the Mapper function of Mapstruct to JMapper.

--- src/main/java/com/ekshunya/sahaaybackend/services/TicketFacade.java
+++ src/main/java/com/ekshunya/sahaaybackend/services/TicketFacade.java
@@ -3,6 +3,7 @@ package com.ekshunya.sahaaybackend.services;
 import com.ekshunya.sahaaybackend.exceptions.BadDataException;
 import com.ekshunya.sahaaybackend.exceptions.DataNotFoundException;
 import com.ekshunya.sahaaybackend.exceptions.InternalServerException;
+import com.ekshunya.sahaaybackend.mapper.MainMapper;
 import com.ekshunya.sahaaybackend.model.daos.Feed;
 import com.ekshunya.sahaaybackend.model.daos.Ticket;
 import com.ekshunya.sahaaybackend.model.daos.TicketType;
@@ -11,6 +12,7 @@ import com.ekshunya.sahaaybackend.model.dtos.TicketDetailsUpdateDto;
 import com.ekshunya.sahaaybackend.model.dtos.TicketDto;
 import com.ekshunya.sahaaybackend.model.dtos.TicketFeedDto;
 import com.google.inject.Inject;
+import com.googlecode.jmapper.JMapper;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,24 +24,27 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class TicketFacade {
     private final TicketService ticketService;
+    private final MainMapper mainMapper;
     private final static String ERROR_MESSAGE = "ERROR : There was an Error while processing the request";
 
-    public TicketFacade(final TicketService ticketService) {
+    public TicketFacade(final TicketService ticketService, final MainMapper mainMapper) {
         this.ticketService = ticketService;
+        this.mainMapper=mainMapper;
     }
 
-    @Inject //TODO there is no Need to first change it to TicketCreateDto -> Ticket -> Bson. Let just change the TicketCreateDtoTo Inherit properties from a base class called Ticket, and can have two-3 Objects which will be the Dto's -> Bson's. Eg: TicketCreatedDto, TicketDetailsDto, MinimalTicketDto to Bson's.
+    @Inject
     public boolean createTicket(@NonNull final TicketCreateDto ticketCreateDto) throws InterruptedException {
         //First time using the Java Fibers. Hopefully its correct.
         ThreadFactory factory = Thread.builder().virtual().factory();
         Future<Boolean> ticketCreatedAck;
         try (var executor = Executors.newThreadExecutor(factory).withDeadline(Instant.now().plusSeconds(2))) {
             ticketCreatedAck = executor.submit(() -> {
-                Ticket ticketToSave = TicketMapper.INSTANCE.ticketCreateDtoToTicket(ticketCreateDto);
+                Ticket ticketToSave = this.mainMapper.ticketCreateDtoToTicket(ticketCreateDto);
                 ticketToSave.setId(UUID.randomUUID());
                 //TODO in the next iteration we will change the call with fibers so that only the DB call is inside one of these. Mapper code can be moved out of fiber.
                 return this.ticketService.createANewTicket(ticketToSave);
@@ -56,9 +61,9 @@ public class TicketFacade {
         Future<TicketDto> ticketUpdatedFuture;
         try (var executor = Executors.newThreadExecutor(factory).withDeadline(Instant.now().plusSeconds(2))) {
             ticketUpdatedFuture = executor.submit(() -> {
-                Ticket ticketToUpdate = TicketMapper.INSTANCE.ticketDetailsUpdateDtoToTicket(ticketDetailsUpdateDto);
+                Ticket ticketToUpdate = this.mainMapper.ticketDetailsUpdateDtoToTicket(ticketDetailsUpdateDto);
                 Ticket updatedTicket = this.ticketService.updateTicket(ticketToUpdate);
-                return TicketMapper.INSTANCE.ticketToTicketDto(updatedTicket);
+                return this.mainMapper.ticketToTicketDto(updatedTicket);//TODO can this seralization be avoided?
             });
             return ticketUpdatedFuture.get();
         } catch (ExecutionException exception) {
@@ -73,7 +78,7 @@ public class TicketFacade {
         try (var executor = Executors.newThreadExecutor(factory).withDeadline(Instant.now().plusSeconds(2))) {
             updatedTicketFuture = executor.submit(() -> {
                 Ticket existingTicket = this.ticketService.fetchTicket(ticketId);
-                return TicketMapper.INSTANCE.ticketToTicketDto(existingTicket);
+                return this.mainMapper.ticketToTicketDto(existingTicket);
             });
             return updatedTicketFuture.get();
         } catch (ExecutionException e) { //TODO This is wrong. We need to catch the indivizual exception that is nested inside the ExecutionException.
@@ -94,7 +99,7 @@ public class TicketFacade {
             double lng = Double.parseDouble(longitude);
             futureTickets = executor.submit(() -> this.ticketService.fetchAllOpenedTicket(actualTicketType, lat, lng));
             List<Ticket> openTickets = futureTickets.get();
-            return TicketMapper.INSTANCE.ticketsToTicketDtos(openTickets);
+            return openTickets.stream().map(this.mainMapper::ticketToTicketDto).collect(Collectors.toList());
         } catch (ExecutionException e) {
             log.error(ERROR_MESSAGE, e);
             if(e.getMessage().contains("com.ekshunya.sahaaybackend.exceptions.DataNotFoundException")){
@@ -109,7 +114,7 @@ public class TicketFacade {
 
     //TODO add unit tests to cover this method.
     public boolean updateTicketWithFeed(@NonNull final TicketFeedDto ticketFeedDto) throws InterruptedException {
-        Feed newFeed = FeedMapper.INSTANCE.ticketFeedDtoToFeed(ticketFeedDto);
+        Feed newFeed =  this.mainMapper.ticketFeedToTicket(ticketFeedDto);
         ThreadFactory factory = Thread.builder().virtual().factory();
         try (var executor = Executors.newThreadExecutor(factory).withDeadline(Instant.now().plusSeconds(2))) {
             Future<Long> ticketFuture = executor.submit(() ->
@@ -149,6 +154,6 @@ public class TicketFacade {
             log.error(ERROR_MESSAGE, e);
             throw new InternalServerException(ERROR_MESSAGE);
         }
-        return TicketMapper.INSTANCE.ticketsToTicketDtos(ticketDtosToReturn);
+        return ticketDtosToReturn.stream().map(this.mainMapper::ticketToTicketDto).collect(Collectors.toList());
     }
 }
