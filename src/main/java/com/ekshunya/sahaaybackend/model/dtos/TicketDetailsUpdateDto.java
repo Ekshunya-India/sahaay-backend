57ea5285e70141c3ff0485d60d79108fdfdbe566
Sunil KS <sunilks.kavali@gmail.com>
2020-11-05 17:36:43 +0530

    Renamed the log-in to ticket-create feature and added ticket-delete, ticket-update and the corresponding json files.

--- src/main/java/com/ekshunya/sahaaybackend/model/dtos/TicketDetailsUpdateDto.java
+++ src/main/java/com/ekshunya/sahaaybackend/model/dtos/TicketDetailsUpdateDto.java
@@ -1,16 +1,10 @@
 package com.ekshunya.sahaaybackend.model.dtos;
 
-import java.time.ZonedDateTime;
-import java.util.List;
-import java.util.Objects;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
 public class TicketDetailsUpdateDto  {
     LocationDto location;
-    List<CommentDto> comments;
     String expectedEnd;
     Integer visibility;
     String ticketAssignedTo;
