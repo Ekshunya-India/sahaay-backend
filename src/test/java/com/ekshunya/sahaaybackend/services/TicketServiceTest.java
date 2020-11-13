6d72a2765be81fd30461e62d423f064d131b2554
Sunil KS <sunilks.kavali@gmail.com>
2020-11-13 12:48:45 +0530

    Added a unit test for the getall method in the service..

--- src/test/java/com/ekshunya/sahaaybackend/services/TicketServiceTest.java
+++ src/test/java/com/ekshunya/sahaaybackend/services/TicketServiceTest.java
@@ -53,7 +53,7 @@ public class TicketServiceTest {
     @Captor
     private ArgumentCaptor<Ticket> ticketArgumentCaptor;
     @Captor
-    private ArgumentCaptor<Document> documentArgumentCaptor;
+    private ArgumentCaptor<Bson> documentArgumentCaptor;
     @Captor
     private ArgumentCaptor<Bson> filtersArgumentCaptor;
     @Captor
@@ -90,24 +90,24 @@ public class TicketServiceTest {
     }
 
     @Test
-    public void aValidCreateTicketGivesBackTrueWhenInserted(){
+    public void aValidCreateTicketGivesBackTrueWhenInserted() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when( ()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(tickets.insertOne(eq(validTicket))).thenReturn(InsertOneResult.acknowledged(null));
 
             sut.createANewTicket(validTicket);
 
-        verify(tickets,times(1)).insertOne(ticketArgumentCaptor.capture());
-        Ticket actualTicket = ticketArgumentCaptor.getValue();
-        assertEquals(validTicket, actualTicket);
-        assertNotNull(validTicket.getId());
+            verify(tickets, times(1)).insertOne(ticketArgumentCaptor.capture());
+            Ticket actualTicket = ticketArgumentCaptor.getValue();
+            assertEquals(validTicket, actualTicket);
+            assertNotNull(validTicket.getId());
         }
     }
 
     @Test
-    public void anyExceptionThrownInCreateIsSentBack(){
+    public void anyExceptionThrownInCreateIsSentBack() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
 
             when(tickets.insertOne(eq(validTicket))).thenThrow(new IllegalStateException("SOME PROBLEM HAPPENED GOLEM"));
             assertThrows(IllegalStateException.class, () -> sut.createANewTicket(validTicket));
@@ -117,21 +117,22 @@ public class TicketServiceTest {
     @Test
     public void updateTicketThrowsBadDataExceptionWhenThereIsAJsonProcessingException() throws JsonProcessingException {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(objectMapper.writeValueAsString(eq(validTicket))).thenThrow(new JsonGenerationException("SOME MESSAGE"));
-        assertThrows(BadDataException.class,()->sut.updateTicket(validTicket));}
+            assertThrows(BadDataException.class, () -> sut.updateTicket(validTicket));
+        }
     }
 
     @Test
     public void upddateTicketSendsDataToMongoDbAndSendsAValidTicketBack() throws JsonProcessingException {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(objectMapper.writeValueAsString(eq(validTicket))).thenReturn(new ObjectMapper().writeValueAsString(validTicket));
             Document mongoDocumentToUpdate = Document.parse(this.objectMapper.writeValueAsString(validTicket));
             sut.updateTicket(validTicket);
-            verify(tickets, times(1)).findOneAndUpdate(eq(Filters.eq("id", uuid)), eq(mongoDocumentToUpdate),optionsArgumentCaptor.capture());
+            verify(tickets, times(1)).findOneAndUpdate(eq(Filters.eq("id", uuid)), eq(mongoDocumentToUpdate), optionsArgumentCaptor.capture());
             FindOneAndUpdateOptions actualOptions = optionsArgumentCaptor.getValue();
-            assertTrue( actualOptions.isUpsert());
+            assertTrue(actualOptions.isUpsert());
             assertEquals(ReturnDocument.AFTER, actualOptions.getReturnDocument());
         }
     }
@@ -139,39 +140,40 @@ public class TicketServiceTest {
     @Test
     public void updateTicketPropagatesExceptionInFindOneAndUpdate() throws JsonProcessingException {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(objectMapper.writeValueAsString(eq(validTicket))).thenReturn(new ObjectMapper().writeValueAsString(validTicket));
             //Document mongoDocumentToUpdate = Document.parse(this.objectMapper.writeValueAsString(validTicket));
-            when(tickets.findOneAndUpdate(eq(Filters.eq("id", uuid)),any(Document.class),any(FindOneAndUpdateOptions.class))) //TODO this needs to chamge. We need to capture the document and verify and also equate the FindOneAndUpdateOptions.
+            when(tickets.findOneAndUpdate(eq(Filters.eq("id", uuid)), any(Document.class), any(FindOneAndUpdateOptions.class))) //TODO this needs to chamge. We need to capture the document and verify and also equate the FindOneAndUpdateOptions.
                     .thenThrow(new IllegalStateException("SOME UNKNOWN EXCEPTION"));
-            assertThrows(IllegalStateException.class,()->sut.updateTicket(validTicket));
+            assertThrows(IllegalStateException.class, () -> sut.updateTicket(validTicket));
         }
     }
 
     @Test
-    public void validIdSentToTheIdIsGivenToMongoDb(){
+    public void validIdSentToTheIdIsGivenToMongoDb() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
-            when(tickets.find(eq(FILTER),eq(Ticket.class))).thenReturn(findIterable);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            when(tickets.find(eq(FILTER), eq(Ticket.class))).thenReturn(findIterable);
             when(findIterable.first()).thenReturn(validTicket);
-        Ticket actualTicket =  sut.fetchTicket(uuid);
-        assertEquals(validTicket,actualTicket);}
+            Ticket actualTicket = sut.fetchTicket(uuid);
+            assertEquals(validTicket, actualTicket);
+        }
     }
 
     //TODO here we are propagating the all DB exceptions out of service layer to Facade. Atleast in Facade we need to capture the DB exceptions.
     @Test
-    public void AnyExceptionThrownByTheMongoCollectionIsPropagatedBack(){
+    public void AnyExceptionThrownByTheMongoCollectionIsPropagatedBack() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(tickets.find(any(Bson.class), eq(Ticket.class))).thenThrow(new IllegalStateException("SOMETHING BAD HAPPENED ALFRED"));
             assertThrows(IllegalStateException.class, () -> sut.fetchTicket(uuid));
         }
     }
 
     @Test
-    public void updateWithFeedUpdatesItInMongoDb() throws JsonProcessingException{
+    public void updateWithFeedUpdatesItInMongoDb() throws JsonProcessingException {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(objectMapper.writeValueAsString(eq(newFeed))).thenReturn(new ObjectMapper().writeValueAsString(newFeed));
             Document feedToAdd = Document.parse(new ObjectMapper().writeValueAsString(newFeed));
             Bson updates = Updates.push("id.feeds.$.", feedToAdd);
@@ -185,7 +187,7 @@ public class TicketServiceTest {
     @Test
     public void updateWithFeedThrowsBadDataExceptionWhenJsonProcessingIsThrown() throws JsonProcessingException {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(objectMapper.writeValueAsString(eq(newFeed))).thenThrow(new JsonMappingException("SOME BAD DATA"));
 
             assertThrows(BadDataException.class, () -> sut.updateWithFeed(newFeed, uuid));
@@ -193,9 +195,9 @@ public class TicketServiceTest {
     }
 
     @Test
-    public void deleteTicketsPassesOnTheIdsToMongoLikeAGoodBoy(){
+    public void deleteTicketsPassesOnTheIdsToMongoLikeAGoodBoy() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             Bson andDeleteBson = and(Filters.eq("id", uuid), Filters.eq("openedBy", uuid), all("state", State.values()));
             when(tickets.deleteOne(eq(andDeleteBson))).thenReturn(deleteResult);
             when(deleteResult.getDeletedCount()).thenReturn(1L);
@@ -205,9 +207,9 @@ public class TicketServiceTest {
     }
 
     @Test
-    public void deleteTicketPropagatesExceptionThrownByMongoDb(){
+    public void deleteTicketPropagatesExceptionThrownByMongoDb() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
-            mocked.when(()-> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             Bson andDeleteBson = and(Filters.eq("id", uuid), Filters.eq("openedBy", uuid), all("state", State.values()));
             when(tickets.deleteOne(eq(andDeleteBson))).thenThrow(new IllegalStateException("SOME THING REALLY BAD"));
             assertThrows(IllegalStateException.class, () -> sut.deleteTicket(uuid, uuid));
@@ -230,7 +232,7 @@ public class TicketServiceTest {
     }
 
     @Test
-    public void whenNoLastValueFoundThenItIsNotUsedInTheQuery(){
+    public void whenNoLastValueFoundThenItIsNotUsedInTheQuery() {
         try (MockedStatic mocked = mockStatic(MongoClients.class)) {
             mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
             when(tickets.find(any(Bson.class))).thenReturn(findIterable);
@@ -241,8 +243,27 @@ public class TicketServiceTest {
             sut.fetchAllTickets(TicketType.PROBLEM, 22.00, 23.00, "created", "NO_VALUE", "30");
             verify(tickets, times(1)).find(documentArgumentCaptor.capture());
             verify(findIterable, times(1)).limit(eq(30));
-            Document filters= documentArgumentCaptor.getValue();
-            assertEquals(3, filters.size());
+            Bson filters = documentArgumentCaptor.getValue();
+            assertTrue("And Filter{filters=[Geometry Operator Filter{fieldName='location', operator='$near', geometry=Point{coordinate=Position{values=[23.0, 22.0]}}, maxDistance=100.0, minDistance=0.0}, Filter{fieldName='ticketType', value=PROBLEM}, {\"created\": 1}]}\n"
+                    .contains(filters.toString()));
         }
+    }
+
+    @Test
+    public void whenValidLastValueFoundThenItIsUsedInTheQuery() {
+        try (MockedStatic mocked = mockStatic(MongoClients.class)) {
+            mocked.when(() -> MongoClients.create(eq(clientSettings))).thenReturn(mongoClient);
+            when(tickets.find(any(Bson.class))).thenReturn(findIterable);
+            when(findIterable.limit(eq(30))).thenReturn(findIterable);
+            when(findIterable.cursor()).thenReturn(mongoCursor);
+            when(mongoCursor.hasNext()).thenReturn(false);
+
+            sut.fetchAllTickets(TicketType.PROBLEM, 22.00, 23.00, "created", "some_last_value", "30");
+            verify(tickets, times(1)).find(documentArgumentCaptor.capture());
+            verify(findIterable, times(1)).limit(eq(30));
+            Bson filters = documentArgumentCaptor.getValue();
+            assertTrue("And Filter{filters=[Geometry Operator Filter{fieldName='location', operator='$near', geometry=Point{coordinate=Position{values=[23.0, 22.0]}}, maxDistance=100.0, minDistance=0.0}, Filter{fieldName='ticketType', value=PROBLEM}, Operator Filter{fieldName='created', operator='$gt', value=some_last_value}, {\"created\": 1}]}\n"
+            .contains(filters.toString()));
         }
+    }
 }
