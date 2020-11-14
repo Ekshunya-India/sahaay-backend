2d00c91e2be13312c54020aa55fb984c0cf091e5
Sunil KS <sunilks.kavali@gmail.com>
2020-11-14 18:40:47 +0530

    Added in comments and link for different processors.

--- src/main/java/com/ekshunya/sahaaybackend/services/AudioProcessor.java
+++ src/main/java/com/ekshunya/sahaaybackend/services/AudioProcessor.java
@@ -1,4 +1,8 @@
 package com.ekshunya.sahaaybackend.services;
 
+//TODO Integrate with Soundcloud, MixCloud, iBroadcast. Mixcloud and iBroadcast is fully free. which is very nice.
+// https://www.mixcloud.com/developers/
+// https://mega.nz/doc  This is a very nice service which is very cheap as well. For 900Rs a month you get 2TB of storage.
+// https://project.ibroadcast.com/
 public class AudioProcessor {
 }
--- src/main/java/com/ekshunya/sahaaybackend/services/ImageProcessor.java
+++ src/main/java/com/ekshunya/sahaaybackend/services/ImageProcessor.java
@@ -1,4 +1,8 @@
 package com.ekshunya.sahaaybackend.services;
 
+//TODO add integration to imgur API, Unsplash API and ImageShack. ImageShack has a android SDK so maybe it is best if we first do the Upload in android and then Send the URL to backend.
+//https://docs.google.com/document/d/16M3qaw27vgwuwXqExo0aIC0nni42OOuWu_OGvpYl7dE/pub
+//https://unsplash.com/developers
+//https://apidocs.imgur.com/?version=latest
 public class ImageProcessor {
 }
