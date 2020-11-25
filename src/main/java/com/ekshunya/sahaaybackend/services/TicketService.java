86f1928600b50efb675d53b74b615f16509cd258
Sunil KS <sunilks.kavali@gmail.com>
2020-11-25 20:22:45 +0530

    Added Keycloack and Jeromq integration to the Project and also added the loom project extension.

--- src/main/java/com/ekshunya/sahaaybackend/services/TicketService.java
+++ src/main/java/com/ekshunya/sahaaybackend/services/TicketService.java
@@ -131,7 +131,6 @@ public class TicketService {
             MongoDatabase db = mongoClient.getDatabase("sahaay-db");
             MongoCollection<Ticket> tickets = db.getCollection("ticket", Ticket.class);
             Document feedToAdd = Document.parse(this.objectMapper.writeValueAsString(newFeed));
-
             return tickets.updateOne(Filters.eq("id",ticketId), Updates.push("id.feeds.$.",feedToAdd)).getMatchedCount();  //TODO the fieldName in Updates.push needs to be looked at properly.
         } catch (JsonProcessingException e) {
             log.error(ERROR);
