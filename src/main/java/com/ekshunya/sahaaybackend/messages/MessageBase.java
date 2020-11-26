047ed070c96831a659bab74012e0f881ecf151e4
Sunil KS <sunilks.kavali@gmail.com>
2020-11-26 13:30:56 +0530

    Added in the userId and priority to the MessageBase. Now we need to properly see how to make the message base properly serializable.

--- src/main/java/com/ekshunya/sahaaybackend/messages/MessageBase.java
+++ src/main/java/com/ekshunya/sahaaybackend/messages/MessageBase.java
@@ -1,8 +1,13 @@
 package com.ekshunya.sahaaybackend.messages;
 
+import com.ekshunya.sahaaybackend.model.daos.Priority;
 import lombok.AllArgsConstructor;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 public class MessageBase {
 	protected final String message;
+	protected final UUID userId;
+	protected final Priority priority;
 }
